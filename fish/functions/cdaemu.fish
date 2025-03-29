0a1f9cfb41ceb9c3e5f8c9cdd1afb63205dad101
Matthew-Miles <matthewmiles430@gmail.com>
2025-03-29 15:53:40 -0400

    adding all dotfiles

---
+++ fish/functions/cdaemu.fish
@@ -0,0 +1,4 @@
+function cdaemu --wraps='cd /Users/Matthew/classes/CDA3101/qemu-arm-img' --description 'alias cdaemu=cd /Users/Matthew/classes/CDA3101/qemu-arm-img'
+  cd /Users/Matthew/classes/CDA3101/qemu-arm-img $argv
+        
+end
